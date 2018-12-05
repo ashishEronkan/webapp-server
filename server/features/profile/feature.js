b59fec07be6263df14804c3f78c6e89ee568650a
Vish Desai <vish@eronkan.com>
2018-12-05 14:18:40 +0530

    Minor changes in how the dashboard works for better usability

--- server/features/profile/feature.js
+++ server/features/profile/feature.js
@@ -41,23 +41,25 @@ class Profile extends PlantWorksBaseFeature {
 	 *
 	 * @summary  Everyone logged-in gets access.
 	 */
-	async getDashboardDisplayDetails(ctxt) {
-		try {
-			const rbacChecker = this._rbac('registered');
-			await rbacChecker(ctxt);
+	async getDashboardDisplayDetails(ctxt) { // eslint-disable-line no-unused-vars
+		return null;
 
-			const defaultDisplay = await super.getDashboardDisplayDetails(ctxt);
+		// try {
+		// 	const rbacChecker = this._rbac('registered');
+		// 	await rbacChecker(ctxt);
 
-			defaultDisplay['attributes']['name'] = `${ctxt.state.user.first_name} ${ctxt.state.user.last_name}`;
-			defaultDisplay['attributes']['description'] = `Edit ${ctxt.state.user.first_name}'s Profile Information`;
-			defaultDisplay['attributes']['icon_type'] = 'img';
-			defaultDisplay['attributes']['icon_path'] = '/profile/get-image';
+		// 	const defaultDisplay = await super.getDashboardDisplayDetails(ctxt);
 
-			return defaultDisplay;
-		}
-		catch(err) {
-			return null;
-		}
+		// 	defaultDisplay['attributes']['name'] = `${ctxt.state.user.first_name} ${ctxt.state.user.last_name}`;
+		// 	defaultDisplay['attributes']['description'] = `Edit ${ctxt.state.user.first_name}'s Profile Information`;
+		// 	defaultDisplay['attributes']['icon_type'] = 'img';
+		// 	defaultDisplay['attributes']['icon_path'] = '/profile/get-image';
+
+		// 	return defaultDisplay;
+		// }
+		// catch(err) {
+		// 	return null;
+		// }
 	}
 	// #endregion
 
